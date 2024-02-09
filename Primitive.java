17d8320b2b7aa0c393dbb6329fcb01c0cc11526a
Ngo Thi Kim Oanh <ntkoanh.orin@outlook.com>
2024-02-09 12:57:57 +0700

    main

---
+++ Primitive.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Primitive {
+    public static void main(String[] args) {
+        // kiêu dữ liệu nguyên thủy
+        int a = 5;
+        boolean b = false;
+        char c = 'a';
+        double d = 4.5;
+        System.out.println(a + " " + b + " " + c + " " + d);
+
+        float x = a;// chuyển kiểu dữ liệu của a tư int sang float
+        System.out.println(x);
+    }
+}
